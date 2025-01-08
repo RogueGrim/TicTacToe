c9cebcf556c6e44891bc6c581f8b41fd5b49d8c0
RogueGrim <equan4843@gmail.com>
2025-01-08 23:51:45 +0530

    completed logic render bugs remaining

--- script.js
+++ script.js
@@ -1,71 +1,168 @@
-var GameBoard = {
+let GameBoard = {
+
+    //array for holding the inputs
     Board: ['','','','','','','','',''],
-    pushIn: function(){
-        this.Board.splice(this.index,1,this.input)
+
+    //function for pushing the inputs into the array 
+    pushIn: function(index,input){
+        this.Board.splice(index,1,input)
     },
-    setInput: function(input,index){
-        this.input = input
-        this.index = index
+    //function to reset the array
+    resetBoard: function(){
+        this.Board = ['','','','','','','','','']
+        console.log(this.Board)
     },
     show: function(){
-        console.log(this.Board);
-    },
-}
-
-function checkWin(){
-    let a = GameBoard.Board
-    if(a[0]==a[1]=='1' && a[1]==a[2]=='1'||a[3]==a[4]=='1' && a[4]==a[5]=='1'||a[6]==a[7]=='1' && a[7]==a[8]=='1'||
-        a[0]==a[3]=='1' && a[3]==a[6]=='1'||a[1]==a[4]=='1'&& a[4]==a[7]=='1'||a[2]==a[5]=='1' && a[5]==a[8]=='1'||
-        a[0]==a[4]=='1' && a[4]==a[8]=='1'||a[2]==a[4]=='1'&& a[4]==a[6]=='1'
-    ){
-        GameController.Players[0].score ++
-        console.log(GameController.Players[0]) 
-        
-    }else if(a[0]==a[1]=='2' && a[1]==a[2]=='2'||a[3]==a[4]=='2' && a[4]==a[5]=='2'||a[6]==a[7]=='2' && a[7]==a[8]=='2'||
-            a[0]==a[3]=='2' && a[3]==a[6]=='2'||a[1]==a[4]=='2' && a[4]==a[7]=='2'||a[2]==a[5]=='2' && a[5]==a[8]=='2'||
-            a[0]==a[4]=='2' && a[4]==a[8]=='2'||a[2]==a[4]=='2'&& a[4]==a[6]=='2'
-    ){
-        GameController.Players[1].score ++
-        console.log(GameController.Players[1])
+        console.log
     }
 }
 
-function GameController(){
 
-    let Players = [
+let gameControl = function(){
+
+    //object holding players
+    let Players  = [
         {
-            name: 'Player1',
+            name: 'Player 1',
             input: 1,
-            score: '',
+            tag:'X',
+            score:0
         },
         {
-            name: 'Player2',
+            name: 'Player 2',
             input: 2,
-            score: '',
+            tag:'O',
+            score:0
         },
     ]
+    let currentPlayer = Players[0]
 
-    let activePlayer = Players[0]
-
-    const switchPlayer = ()=>{
-        activePlayer = activePlayer === Players[0] ? Players[1] : Players[0]
+    //function for changing the varaible to the next player
+    let switchPlayer = function(){
+        currentPlayer = currentPlayer === Players[1] ? Players[0] : Players[1]
     }
 
-    const getActivePlayer = ()=> activePlayer
+    let getActivePlayer = ()=> currentPlayer;
 
-    const playRound = ()=>{
-        let field = prompt('Enter Field')
-        GameBoard.setInput(getActivePlayer().input,field)
-        GameBoard.pushIn()
-        switchPlayer()    
-        checkWin()
+    //function to check win 
+    let checkWin = function(){
+        let a = GameBoard.Board
+        if(a[0]==1 && a[1]==1 && a[2]==1 || a[3]==1 && a[4]==1 && a[5]==1 || a[6]==1 && a[7]==1 && a[8]==1||
+            a[0]==1 && a[3]==1 && a[6]==1 || a[1]==1 && a[4]==1 && a[7]==1 || a[2]==1 && a[5]==1 && a[8]==1||
+            a[0]==1 && a[4]==1 && a[8]==1 || a[2]==1 && a[4]==1 && a[6]==1
+        ){
+            Players[0].score++
+            console.log(`${Players[0].name} Wins!!`) 
+            
+        }else if(a[0]==2 && a[1]==2 && a[2]==2 || a[3]==2 && a[4]==2 && a[5]==2 || a[6]==2 && a[7]==2 && a[8]==2||
+                a[0]==2 && a[3]==2 && a[6]==2 || a[1]==2 && a[4]==2 && a[7]==2 || a[2]==2 && a[5]==2 && a[8]==2||
+                a[0]==2 && a[4]==2 && a[8]==2 || a[2]==2 && a[4]==2 && a[6]==2
+        ){
+            Players[1].score++
+            console.log(`${Players[1].name} Wins!!`)
+        }
     }
-
-//for(i=0;i<5;i++){
-    //playRound()
-//}
-
     
+
+    return {Players,getActivePlayer,switchPlayer,checkWin,}
 }
 
-GameController()
+let updateScreen = function(){
+    let game = gameControl()
+
+    let declareWinner = function(){
+        const display = document.getElementById('container')
+        const plaque = document.createElement('div')
+        plaque.classList.add('plaque')
+        if(game.Players[0].score ==1){
+            plaque.innerText= `${game.Players[0].name} Wins!!`
+            display.appendChild(plaque)
+        }else if(game.Players[1].score ==1){
+            plaque.innerText= `${game.Players[1].name} Wins!!`
+            display.appendChild(plaque)
+        }
+    }   
+
+    let playerTurn = function(){
+            let text = document.querySelector('.announcer')
+            if(game.getActivePlayer() == game.Players[0]){
+                text.innerText = `${game.Players[0].name} 's Turn...`
+            }else{
+                text.innerText = `${game.Players[1].name} "s Turn...`
+            }
+    }
+
+    return {declareWinner,playerTurn}
+
+}
+
+//function to get input value and render input value
+const clickHandler = function(){
+    let game = gameControl()
+    let screen = updateScreen()
+    let container = document.getElementById('container')
+    let area = document.querySelectorAll('.field')
+    let reset = document.querySelector('.reset')
+    let play = document.querySelector('.play')
+
+    //click event for getting input,checking for valid move and pushing in the array
+    let getInput = function(){
+        area.forEach((div)=>{
+            div.addEventListener('click',function(){  
+            let index = div.getAttribute('value') 
+            if(GameBoard.Board[index]!==''){
+                document.querySelector('.announcer').innerText = 'invalid Move'
+                div.innerHTML=''
+            }else{
+                GameBoard.pushIn(index,game.getActivePlayer().input)
+                game.checkWin()
+                screen.declareWinner()
+                screen.playerTurn()
+            }
+            })
+        })
+    }
+
+    //click event for rendering input and switching players
+    let renderInput = function(){
+        area.forEach((render)=>{
+            render.addEventListener('click',function(){
+                const tag = document.createElement('p')
+                tag.classList.add('tag')
+                tag.innerText = game.getActivePlayer().tag
+                render.appendChild(tag)
+                game.switchPlayer()
+            })
+        })
+    }
+
+    //click event for reseting the game
+    let resetGame = function(){
+        reset.addEventListener('click',function(){
+            GameBoard.resetBoard()
+            container.removeChild(container.lastChild)
+            area.forEach((remove)=>{
+                remove.innerHTML=''
+            })
+            game.Players[0].score= 0
+            game.Players[1].score= 0    
+        })
+    }
+
+    //click event for starting the game
+    let startGame = function(){
+        play.addEventListener('click',function(){
+            game.Players[0].name = document.getElementById('player1').value
+            game.Players[1].name = document.getElementById('player2').value
+            getInput()
+            renderInput()
+            resetGame()
+            screen.playerTurn()
+        })
+    }
+
+    return{getInput,renderInput,resetGame,startGame}
+}
+
+let play = clickHandler()
+play.startGame()
